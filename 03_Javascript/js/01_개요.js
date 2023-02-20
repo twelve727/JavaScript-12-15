6dbc854ba923478e09f7efe52e18525bd2733a74
DESKTOP-HFTFEUB\user1 <twelve727@gmail.com>
2023-02-20 15:17:46 +0900

    CSS-JavaScript-01

---
+++ 03_Javascript/js/01_개요.js
@@ -0,0 +1,23 @@
+// 한 줄 주석
+/* 범위 주석 */
+
+// js파일은 <script> 태그 내부라고 생각하면 됨
+
+// btnClick2 (외부)니 js script 여기에 추가구문 만들기!!
+
+// 연결은 필수
+function btnClick2() {
+    alert("external 버튼이 클릭됨");
+}
+
+// div id box의 색깔을 바꿔라 명령하는 구문
+function changeColor1() {
+    document.getElementById("box").style.backgroundColor = "red";
+}
+function changeColor2() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+}
+function changeColor3() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
